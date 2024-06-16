46c78766d51a6be78492ae52c6c3bde9d04e2748
Joel Koch <joel@joelkoch.dev>
2024-06-16 20:58:12 +0200

    Add nix flake for dev shell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "joelkoch.dev website";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.next-ls.url = "github:elixir-tools/next-ls";
+
+  outputs = { self, nixpkgs, flake-utils, next-ls }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages =
+            [
+              pkgs.beam.packages.erlang.elixir
+              # next-ls.packages.${system}.default
+            ];
+        };
+      }
+    );
+}
