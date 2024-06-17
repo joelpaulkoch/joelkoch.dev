7d60c81e1a8393e67ac9b4df2adcf5950cbfc477
Joel Koch <joel@joelkoch.dev>
2024-06-17 21:57:06 +0200

    Add discussions and github links

--- lib/layouts/livebook_layout.ex
+++ lib/layouts/livebook_layout.ex
@@ -4,6 +4,14 @@ defmodule JoelKoch.Dev.LivebookLayout do
 
   def template(assigns) do
     ~H"""
+    <div class="flex">
+      <a href="https://github.com/joelpaulkoch/joelkoch.dev/discussions">
+        <Heroicons.chat_bubble_left_right class="min-h-16" />
+      </a>
+      <a href="https://github.com/joelpaulkoch/joelkoch.dev/blob/main/_livebooks/flax_to_axon_automate.livemd">
+        <img src="/github-mark-white.svg" />
+      </a>
+    </div>
     <article>
       <%= {:safe, render(@inner_content)} %>
     </article>
