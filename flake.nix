631de00c5b3bc9d95fcf6cccb41f45a46fc1ae69
Joel Koch <joel@joelkoch.dev>
2024-07-05 00:06:27 +0200

    Add inotify-tools

--- flake.nix
+++ flake.nix
@@ -4,18 +4,22 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.next-ls.url = "github:elixir-tools/next-ls";
 
-  outputs = { self, nixpkgs, flake-utils, next-ls }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    next-ls,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
         devShell = pkgs.mkShell {
-          packages =
-            [
-              pkgs.beam.packages.erlang.elixir
-              # next-ls.packages.${system}.default
-            ];
+          packages = [
+            pkgs.beam.packages.erlang.elixir
+            pkgs.inotify-tools
+            # next-ls.packages.${system}.default
+          ];
         };
       }
     );
