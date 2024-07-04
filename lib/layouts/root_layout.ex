6c7618b8c3bd10f34856eff252c30591b12f572e
Joel Koch <joel@joelkoch.dev>
2024-07-05 00:06:20 +0200

    Update design

--- lib/layouts/root_layout.ex
+++ lib/layouts/root_layout.ex
@@ -5,7 +5,7 @@ defmodule JoelKoch.Dev.RootLayout do
   def template(assigns) do
     ~H"""
     <!DOCTYPE html>
-    <html lang="en">
+    <html lang="en" class="scroll-smooth">
       <head>
         <meta charset="utf-8" />
         <meta http_equiv="X-UA-Compatible" content="IE=edge" />
@@ -23,8 +23,8 @@ defmodule JoelKoch.Dev.RootLayout do
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </head>
 
-      <body class="min-h-dvh flex flex-col bg-white text-black antialiased dark:bg-neutral-800 dark:text-neutral-200">
-        <main class="grow">
+      <body class="bg-white text-black antialiased dark:bg-neutral-800 dark:text-neutral-200">
+        <main>
           <%= render(@inner_content) %>
         </main>
         <.footer />
@@ -40,31 +40,19 @@ defmodule JoelKoch.Dev.RootLayout do
 
   def footer(assigns) do
     ~H"""
-    <div class="mx-16">
-      <footer class="mx-auto mt-16 flex w-full max-w-7xl shrink-0 flex-col-reverse items-center justify-center gap-4 border-t-2 border-solid border-neutral-800 py-8 transition hover:border-white sm:flex-row">
-        <p class="">
-          © 2024 Joel Koch
-        </p>
-        <nav>
-          <a href="/test" class="rounded-md p-2 ring-white transition hover:ring">
-            Nav somewhere
-          </a>
-        </nav>
-        <nav>
-          <a href="/test" class="rounded-md p-2 ring-white transition hover:ring">
-            Nav somewhere
-          </a>
-        </nav>
-        <nav>
-          <a href="/test" class="rounded-md p-2 ring-white transition hover:ring">
-            Nav somewhere
-          </a>
-        </nav>
-        <p class="">
-          Made with Tableau
-        </p>
-      </footer>
-    </div>
+    <footer class="min-h-20 mx-auto flex w-full max-w-7xl shrink-0 flex-col-reverse items-center justify-center gap-4 border-t-2 border-solid border-neutral-800 py-4 transition hover:border-white sm:flex-row">
+      <p class="">
+        © 2024 Joël Koch
+      </p>
+      <nav>
+        <a href="/test" class="rounded-md p-2 ring-white transition hover:ring">
+          Nav somewhere
+        </a>
+      </nav>
+      <p class="">
+        Made with Tableau
+      </p>
+    </footer>
     """
   end
 end
--- lib/pages/home_page.ex
+++ lib/pages/home_page.ex
@@ -7,61 +7,72 @@ defmodule JoelKoch.Dev.HomePage do
 
   def template(assigns) do
     ~H"""
-    <div class="flex grow flex-col items-center justify-items-start gap-16 pt-64">
-      <div class="mx-16 grid max-w-7xl grid-cols-1 gap-16 sm:grid-cols-2 lg:grid-cols-3">
-        <div class="px-8 sm:px-0">
+    <div class="h-lvh grid grid-rows-4">
+      <div class="row-span-2 row-start-2 mx-auto grid max-w-7xl grid-cols-1 gap-4 px-4 sm:grid-cols-2 sm:gap-16 lg:grid-cols-3">
+        <div class="size-64 mx-auto sm:size-128">
           <img
             class="rounded-3xl object-contain"
             src="https://www.gravatar.com/avatar/a7eb43bdf873c6edcb9d79b0acb21764a78c6983deabba6cbc7fae944645c63c?s=1000"
           />
         </div>
-        <div class="flex flex-col gap-8 pl-16 lg:col-span-2">
-          <h1 class="text-bold text-5xl">
-            It's me
+        <div class="flex flex-col items-center gap-2 sm:items-start sm:gap-8 lg:col-span-2">
+          <h1 class="text-bold text-xl sm:text-5xl">
+            It's me, Joël
           </h1>
           <p>
-            hello, world! What's up?
+            What's up?
           </p>
-          <p>
-            You can check out my github page: github.com/joelpaulkoch
-          </p>
+          <div>
+            <a
+              href="https://github.com/joelpaulkoch"
+              class="text-bold flex w-fit flex-row items-center gap-2 rounded-md p-2 ring-white transition hover:ring sm:gap-4 sm:p-4"
+            >
+              Checkout my GitHub profile <img src="/github-mark.svg" class="size-8 dark:hidden" />
+              <img src="/github-mark-white.svg" class="size-8 hidden dark:block" />
+            </a>
+          </div>
         </div>
       </div>
-      <div class="min-h-60 w-full bg-neutral-900 p-16 ">
-        <div class="mx-auto grid max-w-7xl grid-cols-1 gap-4 sm:grid-cols-2">
-          <div class="s-32 row-span-2 rounded-xl bg-white p-4 dark:bg-neutral-800">
-            <h2 class="bold text-xl">
-              Livebook Action
-            </h2>
+      <div class="row-start-4 mx-auto my-auto">
+        <a href="#livebooks">
+          <Heroicons.chevron_down class="size-16" />
+        </a>
+      </div>
+    </div>
+    <div id="livebooks" class="min-h-60 bg-neutral-900 py-16">
+      <div class="mx-auto grid max-w-7xl grid-cols-1 gap-4 px-4 sm:grid-cols-2">
+        <div class="row-span-2 rounded-xl bg-white p-4 dark:bg-neutral-800">
+          <h2 class="bold text-xl">
+            Livebook Action
+          </h2>
+          <p class="my-2">
+            I've done this thing
+          </p>
+        </div>
+        <div class="s-32 row-span-2 grid grid-cols-3 rounded-xl bg-white p-4 ring-white transition hover:ring dark:bg-neutral-800 sm:col-start-2 sm:row-start-2">
+          <div class="col-span-2">
+            <a href="/livebookthing">
+              <h2 class="bold text-xl">
+                Livebook Action
+              </h2>
+            </a>
             <p class="my-2">
-              I've done this thing
+              I've done this other thing
             </p>
           </div>
-          <div class="s-32 row-span-2 grid grid-cols-3 rounded-xl bg-white p-4 ring-white transition hover:ring dark:bg-neutral-800 sm:col-start-2 sm:row-start-2">
-            <div class="col-span-2">
-              <a href="/livebookthing">
-                <h2 class="bold text-xl">
-                  Livebook Action
-                </h2>
-              </a>
-              <p class="my-2">
-                I've done this other thing
-              </p>
-            </div>
-            <div class="col-start-3 m-auto rounded-md ring-white hover:ring">
-              <a href="https://livebook.dev/run?url=test">
-                <img src="https://livebook.dev/badge/v1/blue.svg" alt="Run in Livebook" />
-              </a>
-            </div>
+          <div class="col-start-3 m-auto ">
+            <a href="https://livebook.dev/run?url=test">
+              <img src="https://livebook.dev/badge/v1/blue.svg" alt="Run in Livebook" />
+            </a>
           </div>
-          <div class="s-32 row-span-2 rounded-xl bg-white p-4 dark:bg-neutral-800">
-            <h2 class="bold text-xl">
-              Livebook Action
-            </h2>
-            <p class="my-2">
-              I've done this thing
-            </p>
-          </div>
+        </div>
+        <div class="s-32 row-span-2 rounded-xl bg-white p-4 dark:bg-neutral-800">
+          <h2 class="bold text-xl">
+            Livebook Action
+          </h2>
+          <p class="my-2">
+            I've done this thing
+          </p>
         </div>
       </div>
     </div>
