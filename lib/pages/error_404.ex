c9078fe7debe79dd4c8392f84cff3af22376ebd4
Joel Koch <joel@joelkoch.dev>
2024-08-18 23:12:57 +0200

    Upgrade 404 page

--- lib/pages/error_404.ex
+++ lib/pages/error_404.ex
@@ -7,7 +7,18 @@ defmodule JoelKoch.Dev.Error404 do
 
   def template(assigns) do
     ~H"""
-    This page does not exist
+    <div class="text-pretty grid h-full place-content-center px-4 text-center text-2xl">
+      <p>
+        This page does not exist.
+      </p>
+
+      <a
+        href="/"
+        class="rounded-md p-2 underline ring-white transition hover:text-bold hover:decoration-2 hover:ring"
+      >
+        Go back to home.
+      </a>
+    </div>
     """
   end
 end
