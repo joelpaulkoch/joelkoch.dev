ef361e208f93681ce34d4b12a51845a614e00810
Joel Koch <joel@joelkoch.dev>
2024-09-29 12:39:19 +0200

    Resize livebook layout header

--- lib/layouts/livebook_layout.ex
+++ lib/layouts/livebook_layout.ex
@@ -4,20 +4,20 @@ defmodule JoelKoch.Dev.LivebookLayout do
 
   def template(assigns) do
     ~H"""
-    <div class="flex flex-row justify-between">
+    <div class="grid grid-cols-2">
       <a
         :if={@page[:livebook_url]}
         href={"https://livebook.dev/run?url=#{URI.encode(@page.livebook_url)}"}
       >
         <img src="https://livebook.dev/badge/v1/blue.svg" alt="Run in Livebook" />
       </a>
-      <div class="flex grow justify-end">
+      <div class="flex items-center justify-end gap-4">
         <a :if={@page[:discussions_url]} href={@page.discussions_url}>
-          <Heroicons.chat_bubble_left_right class="size-16" />
+          <Heroicons.chat_bubble_left_right class="size-8" />
         </a>
         <a :if={@page[:livebook_url]} href={@page.livebook_url}>
-          <img src="/github-mark.svg" class="dark:hidden" />
-          <img src="/github-mark-white.svg" class="hidden dark:block" />
+          <img src="/github-mark.svg" class="size-8 dark:hidden" />
+          <img src="/github-mark-white.svg" class="size-8 hidden dark:block" />
         </a>
       </div>
     </div>
